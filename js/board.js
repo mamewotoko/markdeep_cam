49a777e1c40b1c1cd6dcb6b30329fee98c9fd1a3
Takashi Masuyama <mamewotoko@gmail.com>
2020-08-15 21:47:10 +0900

    support mac command key

--- js/board.js
+++ js/board.js
@@ -194,7 +194,7 @@ function board_init(){
         //console.log(event.key);
         var keyname = event.key;
         //Ctrl or Cmd
-        var ctrl = event.getModifierState('Control') || (event.keyCode == 91);
+        var ctrl = event.getModifierState('Control') || (keyname == "[");
         //
 
         if(keyname in color_table){
