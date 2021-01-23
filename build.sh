c7b50a67b187266e49045703232dda853fae4fac
Takashi Masuyama <mamewotoko@gmail.com>
2021-01-23 21:22:57 +0900

    add full.html

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ mkdir -p build lib/third/css lib/third/js
 
 cp ./node_modules/jquery/dist/jquery.min.js lib/third/js
 cp ./node_modules/bootstrap/dist/js/bootstrap.min.js lib/third/js
-cp ./node_modules/svg.js/dist/svg.min.js lib/third/js
-cp ./node_modules/svg.draw.js/dist/svg.draw.min.js lib/third/js
+#cp ./node_modules/svg.js/dist/svg.min.js lib/third/js
+#cp ./node_modules/svg.draw.js/dist/svg.draw.min.js lib/third/js
 cp ./node_modules/iscroll/build/iscroll.js lib/third/js
 cp ./node_modules/bootstrap/dist/css/bootstrap.min.css lib/third/css
 
