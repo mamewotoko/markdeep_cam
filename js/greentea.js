194efe4c3189b6f9c9d11244df8ac6c9fe6a40c1
mamewotoko <mamewotoko@users.noreply.github.com>
2021-02-03 11:59:18 +0000

    deploy: 7c77ad44e9016754d55d036b3e8c0bc86dc2cfe0

--- js/greentea.js
+++ js/greentea.js
@@ -32,7 +32,7 @@ window.init_greentea = function(){
     //macでない場合どうなる?
     xc.font = "82pt ヒラギノ角ゴ";
     xc.fillText("？", 0, canvas.width * 0.8);
-
+    
     for(var i = 0; i < 3; i++){
         var geometry = new THREE.BoxGeometry(32, 32, 32);
         var texture = new THREE.Texture(canvas);
@@ -44,6 +44,46 @@ window.init_greentea = function(){
         scene.add(obj);
     }
 
+    scene.add(buildAxes(2000));
+    //grid
+    var grid_interval = 40;
+
+    for(var i = 1; i < 100; i++){
+        [
+            [grid_interval*i, 2000, 0, 1],
+            [0, grid_interval*i, 2000, 2],
+            [2000, 0, grid_interval*i, 0],
+            [2000, grid_interval*i, 0, 0],
+            [0, 2000, grid_interval*i, 1],
+            [grid_interval*i, 0, 2000, 2],
+        ].forEach(k => {
+            var x = k[0];
+            var y = k[1];
+            var z = k[2];
+            var mat = new THREE.LineBasicMaterial({ linewidth: 3, color: 0xFFFFFF });
+
+            var xx = x;
+            var yy = y;
+            var zz = z;
+            if(k[3] == 0){
+                xx = 0;
+            }
+            else if(k[3] == 1){
+                yy = 0;
+            }
+            else {
+                zz = 0;
+            }
+            console.log(x, y, z, xx, yy, zz);
+            var src = new THREE.Vector3(x, y, z);
+            var dst = new THREE.Vector3(xx, yy, zz);
+            var geom = new THREE.Geometry();
+            geom.vertices.push(src);
+            geom.vertices.push(dst);
+            scene.add(new THREE.Line(geom, mat, THREE.LineSegments ));
+        });
+    }
+   
     camera.position.set(0, 40, 30);
 
     window.greentea_loop = function() {
